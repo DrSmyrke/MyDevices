2110974a9c886b56c9aabd816d67d0d28dd37907
Jura Prokofev <Smyrke2005@yandex.ru>
2022-06-12 13:03:00 +0300

    Added AP_configure

---
+++ AP_configure/include/functions.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_FUNCTIONS_H_
+#define _MAIN_FUNCTIONS_H_
+
+#include "main.h"
+
+//-------------------------------------------------------------------------------
+
+
+
+
+//-------------------------------------------------------------------------------
+bool WiFiConnectionState(void);
+void wifi_AP_init(void);
+
+
+
+
+
+#endif //_MAIN_FUNCTIONS_H_
---
+++ AP_configure/include/http.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_HTTP_H_
+#define _MAIN_HTTP_H_
+
+
+//-------------------------------------------------------------------------------
+
+
+
+
+//-------------------------------------------------------------------------------
+void setNoCacheContent(void);
+void redirect(void);
+void handleRoot(void);
+void handleWifiConfig(void);
+void handleNotFound(void);
+
+
+
+
+
+#endif //_MAIN_HTTP_H_
