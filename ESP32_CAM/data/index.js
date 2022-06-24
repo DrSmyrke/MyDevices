3d62b8ad6c7a4271c04de01121725bec76dff22f
Jura Prokofev <Smyrke2005@yandex.ru>
2022-06-24 13:00:47 +0300

    Added ESP32_CAM

---
+++ ESP32_CAM/data/index.js
@@ -0,0 +1,166 @@
+//-----------------------------------------------------------------------------
+var app = {
+	"debug": false,
+	"version": 99999,
+};
+
+//-----------------------------------------------------------------------------
+function makeHttpObject()
+{
+	try {return new XMLHttpRequest();}
+	catch (error) {}
+	try {return new ActiveXObject("Msxml2.XMLHTTP");}
+	catch (error) {}
+	try {return new ActiveXObject("Microsoft.XMLHTTP");}
+	catch (error) {}
+	throw new Error("Could not create HTTP request object.");
+}
+
+//-----------------------------------------------------------------------------
+window.onload = function(){
+	if( document.location.pathname == "/" ){
+		buildUpdatePage();
+	}
+
+	updateData();
+};
+
+
+
+//-----------------------------------------------------------------------------
+function updateCaptureImg()
+{
+	let img = document.getElementById( 'captureImg' );
+	if( img == undefined ){
+		console.error( "captureImg is undefined" );
+		return;
+	}
+
+	let timestamp = new Date().getTime();
+	img.src = 'http://' + document.location.hostname  + '/capture?t=' + timestamp;
+}
+
+//-----------------------------------------------------------------------------
+function buildIndexPage()
+{
+	let obj = document.getElementById( 'contentBox' );
+	if( obj == undefined ){
+		console.error( "contentBox is undefined" );
+		return;
+	}
+	
+	obj.innerHTML = '<div class="camBox"><fieldset><legend>Capture <a href="javascript:void( 0 );" onClick="updateCaptureImg();">Update</a></legend><img id="captureImg" src="http://' + document.location.hostname  + '/capture"></fieldset><fieldset><legend>Stream</legend><img src="http://' + document.location.hostname  + ':81/stream"></fieldset></div>';
+}
+
+//-----------------------------------------------------------------------------
+function buildUpdatePage()
+{
+	let obj = document.getElementById( 'contentBox' );
+	if( obj == undefined ){
+		console.error( "contentBox is undefined" );
+		return;
+	}
+
+	obj.innerHTML = '<input type="button" value="Check update" onClick="checkUpdate();"> <span id="newVersionL"><span>';
+}
+
+//-----------------------------------------------------------------------------
+function updateData()
+{
+
+	var request = makeHttpObject();
+	request.open( "GET", "/get", true );
+	request.send( null );
+
+	request.onreadystatechange = function(){
+		if( request.readyState == 4 ){
+			if( request.status == 200 ){
+				if( app.debug ) console.log( "updateData >:", request.responseText );
+				let dataObject = JSON.parse( request.responseText );
+
+				if( dataObject.hasOwnProperty( "version" ) ){
+					let vLabel = document.getElementById( 'versionL' );
+					if( vLabel != undefined ){
+						vLabel.innerHTML = "v" + dataObject.version;
+						app.version = Number( dataObject.version ).toFixed();
+					}
+				}
+			}
+		}
+	};
+}
+
+//-----------------------------------------------------------------------------
+function checkUpdate()
+{
+	let obj = document.getElementById( 'newVersionL' );
+	if( obj == undefined ){
+		console.error( "newVersionL is undefined" );
+		return;
+	}
+
+	obj.innerHTML = 'Loading...';
+
+	var request = makeHttpObject();
+	request.open( "GET", "/update?cmd=check_update", true );
+	request.send( null );
+
+	request.onreadystatechange = function(){
+		if( request.readyState == 4 ){
+			if( request.status == 200 ){
+				if( app.debug ) console.log( "checkUpdate >:", request.responseText );
+				let dataObject = JSON.parse( request.responseText );
+
+				if( dataObject.hasOwnProperty( "newVersion" ) ){
+					obj.innerHTML = "Firmvare available: v" + dataObject.newVersion;
+					let newVersion = Number( dataObject.newVersion ).toFixed();
+					if( newVersion > app.version ){
+						obj.innerHTML += ' <input type="button" value="UPDATE" onClick="updateDevice();">';
+					}
+				}
+			}else if( request.status == 401 ){
+				obj.innerHTML = request.responseText;
+			}else{
+				obj.innerHTML = "Unknown Error";
+			}
+		}
+	};
+}
+
+//-----------------------------------------------------------------------------
+function updateDevice()
+{
+	let obj = document.getElementById( 'contentBox' );
+	if( obj == undefined ){
+		console.error( "contentBox is undefined" );
+		return;
+	}
+
+	obj.innerHTML = 'Updating...';
+
+	var request = makeHttpObject();
+	request.open( "GET", "/update?cmd=update", true );
+	request.send( null );
+
+	request.onreadystatechange = function(){
+		if( request.readyState == 4 ){
+			if( request.status == 200 ){
+				if( app.debug ) console.log( "updateDevice >:", request.responseText );
+				obj.innerHTML = "Success";
+				document.location.reload();
+			}
+		}else if( request.status == 401 ){
+			obj.innerHTML = request.responseText;
+		}else{
+			obj.innerHTML = "Unknown Error";
+		}
+	};
+}
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
---
+++ ESP32_CAM/include/main.h
@@ -0,0 +1,39 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+
+//-------------- DEFINES ------------------------------------------------
+#define LED_PIN											GPIO_NUM_33
+#define FLASH_LED_PIN									GPIO_NUM_4
+#define FLASH_LED_PWM_CHANNEL							0
+#define DEVICE_NAME										"ESP32_HOME_CAM"
+#define WEB_PAGE_BUFF_SIZE								2048
+#define FIRMWARE_REPOSITORY								"http://firmware.drsmyrke.keenetic.pro/esp32_cam"
+#define ADMIN_LOGIN										"admin"
+#define ADMIN_PASSWORD									"admin"
+
+// Select camera model
+// #define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+// #define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+
+//-------------- INCLUDES -----------------------------------------------
+#include "camera_pins.h"
+#include <Arduino.h>
+#include <esp_functions.h>
+#include <SPIFFS.h>
+#include "http.h"
+#include "functions.h"
+
+//------------------- VARIABLES ---------------------------
+extern WebServer webServer;
+extern char tmpVal[ 12 ];
+extern char pageBuff[ WEB_PAGE_BUFF_SIZE ];
+
+
+#endif //__MAIN_H_
